7dd01776ce7e1933a8a49bf157ffa4f64a02d837
erwin <browererwin@gmail.com>
2023-09-06 17:39:33 +0300

    Initial commit

---
+++ src/test/java/com/example/tacos/Demo1ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest                 // <1>
+public class Demo1ApplicationTest {
+
+    @Test                         // <2>
+    public void contextLoads() {
+    }
+
+}
